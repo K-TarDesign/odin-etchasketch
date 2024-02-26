361e40440678e9564a7ba27295cc3dee9046e7c0
Kari T <131820486+K-TarDesign@users.noreply.github.com>
2024-02-26 09:13:31 -1000

    Develop JavaScript

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 // VARIABLES
 
-    // GRID
-    const gridDims;
+    // GRID - GLOBAL VARIABLES
+    const gridDims = 960;   // Grid dimensions in pixels
+    let boxDims;            // Size of divisions in pixels
 
-    let divs;
-    let boxDims;
+    // GRID - DOM VARIABLES
+    const grid = document.querySelector('#grid');
+
+
+
+// FUNCTIONS
+
+    function checkDivInput(value) {
+        
+        if (typeof(value) != "number" || value < 1 || value > 100) {
+            let x = prompt("Please enter a value between 1 and 100", 16);
+            ;
+        } else {
+            createDivDims(value);
+        }
+
+    }
+
+    function createDivDims(divs) {
+
+        
+        boxDims = Math.ceil( gridDims / divs );
+    }
